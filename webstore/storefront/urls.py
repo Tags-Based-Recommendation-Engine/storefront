0f1a472d8d27528d9e3d6e5c2dd2b0444041e7c9
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-03-02 16:48:59 +0530

    Seller Profile: added url to urls.py and index

--- webstore/storefront/urls.py
+++ webstore/storefront/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/seller/1', views.registerUSeller, name='reguseller'),
     path('register/seller/2', views.registerSeller, name='regseller'),
+    path('seller/profile', views.sellerProfile, name='sellerprofile'),
 
 
 
