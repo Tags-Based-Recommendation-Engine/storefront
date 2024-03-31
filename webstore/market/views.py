2c893f0fc1ae7fc3ff2731d6d9b6eb1070632a28
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-03-31 13:37:35 +0530

    STOREFRONT: WIP Home page

--- webstore/market/views.py
+++ webstore/market/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout , authenticate
 from django.contrib.auth.decorators import login_required
+from storefront.models import Product, Category
 
 
 def index(request):
-    context = {}
+    product = Product.objects.all()
+    Catagory = Category.objects.all()
+    context = {
+        'product':product,
+        'catagory' : Catagory
+    }
     return render(request, 'market/index.html', context)
 
 def search(request):
