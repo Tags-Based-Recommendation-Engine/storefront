11fce69018633e11db2f09a156bafa2cda85f6f5
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-04-10 20:50:49 +0530

    Bug Fix: URls

--- webstore/webstore/urls.py
+++ webstore/webstore/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('seller', include('storefront.urls')),
+    path('seller/', include('storefront.urls')),
     path('', include('market.urls')),
 
 ]
