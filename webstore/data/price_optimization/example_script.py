8e5d7f06eac87fdeb8c4ebe8d68fe6e6366464eb
Varad Joshi <varad.gcs@gmail.com>
2024-04-11 20:31:03 +0530

    data: Fixup predictions

--- webstore/data/price_optimization/example_script.py
+++ webstore/data/price_optimization/example_script.py
@@ -8,10 +8,7 @@ model = load_model('discount_model.h5')
 def get_optimized_price(inventory, min_price, max_price, rating, strategy, user_interest):
     input_data = np.array([[inventory, min_price, max_price, rating, strategy]])
     discount = model.predict(input_data)[0][0]
-    print(discount)
-    if user_interest!=0:
-        discount -= (0.15*discount*(user_interest))
-        print(discount)
+    discount -= (0.15*discount*(user_interest))
     predicted_price = max_price-discount
     optimized_price = int(min(max(predicted_price,min_price),max_price))
     return user_interest, optimized_price
